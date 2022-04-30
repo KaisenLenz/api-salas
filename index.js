48ca0722e845c73ff0e509b0fece6427cf205b26
KaisenLenz <fmatus@alumnosuls.cl>
2022-04-29 22:52:43 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+
+
+app.get('/',(req,res)=>{
+    console.log('[TEST]!');
+    res.send('Hello from Homepage');
+    
+});
+
+app.listen(PORT, ()=> console.log(`Server Running on port: http://localhost:${PORT}`));
