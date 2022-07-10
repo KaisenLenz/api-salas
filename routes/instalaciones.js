20bd36c206ea58da8bda261f2dd60715e68dc1ee
KaisenLenz <fmatus@alumnosuls.cl>
2022-07-10 10:43:18 -0400

    Actualizacion de Endpoints Instalaciones

--- routes/instalaciones.js
+++ routes/instalaciones.js
@@ -1,5 +1,5 @@
 const aws = require("aws-sdk")
-const multer =require("multer")
+const multer = require("multer")
 const multerS3 = require("multer-s3")
 const express = require("express");
 
@@ -15,96 +15,58 @@ const secretAccessKey = process.env.AWS_BUCKET_SECRET_KEY;
 
 
 aws.config.update({
-    secretAccessKey: secretAccessKey,
-    accessKeyId: accessKeyID,
-    region: region //your bucket region
-    })
+  secretAccessKey: secretAccessKey,
+  accessKeyId: accessKeyID,
+  region: region //your bucket region
+})
 
 const s3 = new aws.S3()
 
-const upload=multer({
-    storage: multerS3({
-        s3,
-        bucket: bucketName, //upload to images folder
-        acl:'public-read',
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-          },
-          key: function (req, file, cb) {
-            cb(null, Date.now().toString() + "_" + file.originalname);
-          },
- }),
-    limits:{
-       fileSize: 3000000 // Maximum 1 MB
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: bucketName, //upload to images folder
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
     },
-    fileFilter(req,file,cb){
-      //filter out the file that we doesn't want to upload   
-      if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){ 
-          // only valid image formats are allowed to upload
-         return cb(new Error('Please upload an Image '))
-       }
-       cb(undefined,true) //pass 'flase' if u want to reject upload
-    }})
-    
-
-app.post('/upload', upload.single("image"), async (req, res) => {
-    try {
-        res.send({
-         'url':req.file.location
-         })
-    } catch(error){ 
-        res.status(400).send({error:error.message})
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + "_" + file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 3000000 // Maximum 1 MB
+  },
+  fileFilter(req, file, cb) {
+    //filter out the file that we doesn't want to upload   
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      // only valid image formats are allowed to upload
+      return cb(new Error('Please upload an Image '))
     }
-    
+    cb(undefined, true) //pass 'flase' if u want to reject upload
+  }
 })
 
-/*
-app.get('/get/:filename', async (req,res)=>{
-    const filename = req.params.filename
-    console.log("Entra");
-    let x = await s3.getObject({Bucket:bucketName,Key:filename}).promise();
 
-    res.send(x.Body);
-})*/
+app.post('/upload', upload.single("image"), async (req, res) => {
+  try {
+    res.send({
+      'url': req.file.location
+    })
+  } catch (error) {
+    res.status(400).send({ error: error.message })
+  }
 
-/*
-app.delete('/delete/:filename', async (req,res)=>{
-    const filename = req.params.filename
-    console.log("Entra");
-    await s3.deleteObject({Bucket:bucketName,Key:filename}).promise();
+})
 
-    res.send("Se Elimino Exitosamente");
-})*/
 
 /*Obtener todas las instalaciones */
 //http://18.231.149.121:3000/instalacion
-app.get('/', async (req,res)=>{
-  
+app.get('/', async (req, res) => {
+
   db.any(`SELECT instalacion.id_instalacion,instalacion.nombre,tipoinstalacion.nombre as tipoinstalacion,sector.nombre as sector,campus.nombre as campus, instalacion.descripcion, instalacion.piso,instalacion.latitud,instalacion.longitud,instalacion.foto,instalacion.id_usuario_creador,instalacion.fecha_creacion
           FROM instalacion, sector, campus, tipoinstalacion
           WHERE sector.id_campus = campus.id_campus AND instalacion.id_sector = sector.id_sector AND instalacion.id_tipo = tipoinstalacion.id_tipo;`)
-  .then((instalaciones) => {
-    res.status(200).json({
-      ok: true,
-      instalaciones: instalaciones,
-    });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      ok: false,
-      mensaje: "Error agregando",
-      errors: err,
-    });
-  });  
-})
-
-/*Obtener todas las instalaciones por nivel */
-//http://18.231.149.121:3000/instalacion/getPiso?piso=2
-app.get('/getPiso', async (req,res)=>{
-    const piso = req.query.piso
-    db.any(`SELECT instalacion.id_instalacion,instalacion.nombre,tipoinstalacion.nombre as tipoinstalacion,sector.nombre as sector,campus.nombre as campus, instalacion.descripcion, instalacion.piso,instalacion.latitud,instalacion.longitud,instalacion.foto
-            FROM instalacion, sector, campus, tipoinstalacion
-            WHERE sector.id_campus = campus.id_campus AND instalacion.id_sector = sector.id_sector AND instalacion.id_tipo = tipoinstalacion.id_tipo AND instalacion.piso = $1 ;`,piso)
     .then((instalaciones) => {
       res.status(200).json({
         ok: true,
@@ -117,57 +79,89 @@ app.get('/getPiso', async (req,res)=>{
         mensaje: "Error agregando",
         errors: err,
       });
-    });  
+    });
+})
+
+/*Obtener todas las instalaciones por nivel */
+//http://18.231.149.121:3000/instalacion/getPiso?piso=2
+app.get('/getPiso', async (req, res) => {
+  const piso = req.query.piso
+  db.any(`SELECT instalacion.id_instalacion,instalacion.nombre,tipoinstalacion.nombre as tipoinstalacion,sector.nombre as sector,campus.nombre as campus, instalacion.descripcion, instalacion.piso,instalacion.latitud,instalacion.longitud,instalacion.foto
+            FROM instalacion, sector, campus, tipoinstalacion
+            WHERE sector.id_campus = campus.id_campus AND instalacion.id_sector = sector.id_sector AND instalacion.id_tipo = tipoinstalacion.id_tipo AND instalacion.piso = $1 ;`, piso)
+    .then((instalaciones) => {
+      res.status(200).json({
+        ok: true,
+        instalaciones: instalaciones,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error No se encuentra el piso seleccionado",
+        errors: err,
+      });
+    });
 })
 
 /*Obtener instalaciones por id */
 //http://18.231.149.121:3000/instalacion/getId?id=2
-app.get('/getId', async (req,res)=>{
+app.get('/getId', async (req, res) => {
   const id = req.query.id
   db.any(`SELECT instalacion.id_instalacion,instalacion.nombre,tipoinstalacion.nombre as tipoinstalacion,sector.nombre as sector,campus.nombre as campus, instalacion.descripcion, instalacion.piso,instalacion.latitud,instalacion.longitud,instalacion.foto
           FROM instalacion, sector, campus, tipoinstalacion
-          WHERE sector.id_campus = campus.id_campus AND instalacion.id_sector = sector.id_sector AND instalacion.id_tipo = tipoinstalacion.id_tipo AND instalacion.id_instalacion = $1 ;`,id)
-  .then((instalaciones) => {
-    res.status(200).json({
-      ok: true,
-      instalaciones: instalaciones,
+          WHERE sector.id_campus = campus.id_campus AND instalacion.id_sector = sector.id_sector AND instalacion.id_tipo = tipoinstalacion.id_tipo AND instalacion.id_instalacion = $1 ;`, id)
+    .then((instalaciones) => {
+      res.status(200).json({
+        ok: true,
+        instalaciones: instalaciones,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error 172: Error en la Consulta",
+        errors: err,
+      });
     });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      ok: false,
-      mensaje: "Error 172: Error en la Consulta",
-      errors: err,
-    });
-  });  
 })
 
 /*Obtener todas las instalaciones por nombre */
-app.get('/filtro', async (req,res)=>{
+//http://18.231.149.121:3000/instalacion/filtro?nombre=206&id_campus=1&id_tipo=1
+app.get('/filtro', async (req, res) => {
   const query = req.query;
   const nombre = query.nombre;
   const id_campus = query.id_campus;
   const id_tipo = query.id_tipo;
-  console.log(query);
 
-  if(nombre != undefined){
-    res.status(200).json({
-      ok: true,
-      mensaje: "Se Filtro " + nombre
+
+  db.any(`SELECT ins.id_instalacion,ins.nombre,tipo.nombre as tipoinstalacion,sec.acronimo as sector,cam.acronimo as campus, ins.descripcion, ins.piso,ins.latitud,ins.longitud,ins.foto
+            FROM instalacion as ins 
+            INNER JOIN sector as sec ON ins.id_sector = sec.id_sector 
+            INNER JOIN campus as cam ON sec.id_campus = cam.id_campus
+            INNER JOIN tipoinstalacion as tipo ON ins.id_tipo = tipo.id_tipo
+            WHERE ins.nombre = $1 AND ins.id_tipo = $2 AND sec.id_campus = $3;`, [nombre, id_tipo, id_campus])
+    .then((instalaciones) => {
+      res.status(200).json({
+        ok: true,
+        instalaciones: instalaciones,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error en la consulta",
+        errors: err,
+      });
     });
-  }
-  else{
-    res.status(200).json({
-      ok: true,
-      mensaje: "Error no ha ingresado los parametros de la consulta"
-    });
-  }
- 
+
+
+
 })
 
 /*Insertar instalaciones*/
 //http://18.231.149.121:3000/instalacion/insert
-app.post('/insert',  upload.single("image"), async (req,res)=>{
+app.post('/insert', upload.single("image"), async (req, res) => {
   const body = req.body;
   const nombre = body.nombre;
   const id_sector = body.id_sector;
@@ -183,24 +177,24 @@ app.post('/insert',  upload.single("image"), async (req,res)=>{
     text: "INSERT INTO instalacion (nombre,id_sector,id_tipo,descripcion,piso,latitud,longitud,foto,id_usuario_creador) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id_instalacion;",
     values: [nombre, id_sector, id_tipo, descripcion, piso, latitud, longitud, foto, id_usuario],
   });
-  
+
   try {
     db.one(agregaInstalacion)
-    .then((instalaciones) => {
-      res.status(200).json({
-        ok: true,
-        mensaje: "Se agregado Exitosamente en el id "+instalaciones.id_instalacion
+      .then((instalaciones) => {
+        res.status(200).json({
+          ok: true,
+          mensaje: "Se agregado Exitosamente en el id " + instalaciones.id_instalacion
+        });
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          ok: false,
+          mensaje: "Error agregando",
+          errors: err,
+        });
       });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        ok: false,
-        mensaje: "Error agregando",
-        errors: err,
-      });
-    });  
-  } catch(error){ 
-    res.status(400).send({error:error.message})
+  } catch (error) {
+    res.status(400).send({ error: error.message })
   }
 
 })
@@ -233,70 +227,70 @@ app.put("/update", (req, res, next) => {
 
 /*Cambiar Query por Body y dejarlo el link solo como / */
 //http://18.231.149.121:3000/instalacion/delete
-app.delete('/delete', async (req,res)=>{
+app.delete('/delete', async (req, res) => {
   const id = req.body.id
   db.one("DELETE FROM instalacion WHERE id_instalacion = $1 RETURNING foto", id)
-  .then((instalacion) => {
-    const foto = instalacion.foto;
-    const imagen = foto.split('/');
-    const filename = imagen[3]
-    console.log(filename);
-    s3.deleteObject({Bucket:bucketName,Key:filename}).promise();
-    res.status(200).json({
-      ok: true,
-      mensaje: "Se ha borrado exitosamente "+instalacion.foto,
+    .then((instalacion) => {
+      const foto = instalacion.foto;
+      const imagen = foto.split('/');
+      const filename = imagen[3]
+      console.log(filename);
+      s3.deleteObject({ Bucket: bucketName, Key: filename }).promise();
+      res.status(200).json({
+        ok: true,
+        mensaje: "Se ha borrado exitosamente " + instalacion.foto,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error agregando",
+        errors: err,
+      });
     });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      ok: false,
-      mensaje: "Error agregando",
-      errors: err,
-    });
-  });  
 })
 
 /*Obtener Todos los Tipos de Salas */
 //http://18.231.149.121:3000/instalacion/getTipo
-app.get('/getTipo', async (req,res)=>{
-  
+app.get('/getTipo', async (req, res) => {
+
   db.any(`SELECT * FROM tipoinstalacion;`)
-  .then((instalaciones) => {
-    res.status(200).json({
-      ok: true,
-      instalaciones: instalaciones,
+    .then((instalaciones) => {
+      res.status(200).json({
+        ok: true,
+        instalaciones: instalaciones,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error agregando",
+        errors: err,
+      });
     });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      ok: false,
-      mensaje: "Error agregando",
-      errors: err,
-    });
-  });  
 })
 
 /*Obtener Todos los Sectores perteneciente al id de Campus */
 //http://18.231.149.121:3000/instalacion/getSectores?id=2
-app.get('/getSectores', async (req,res)=>{
+app.get('/getSectores', async (req, res) => {
   const id = req.query.id
   db.any(`SELECT s.id_sector, ca.nombre as nombre_campus, s.nombre as nombre_sector, s.acronimo
           FROM sector as s
           INNER JOIN campus as ca ON s.id_campus = ca.id_campus 
           WHERE ca.id_campus = $1;`, id)
-  .then((instalaciones) => {
-    res.status(200).json({
-      ok: true,
-      instalaciones: instalaciones,
+    .then((instalaciones) => {
+      res.status(200).json({
+        ok: true,
+        instalaciones: instalaciones,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error agregando",
+        errors: err,
+      });
     });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      ok: false,
-      mensaje: "Error agregando",
-      errors: err,
-    });
-  });  
 })
 
 
